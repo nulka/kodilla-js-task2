6e40d6d021f95d69db1aea79d502c75373a3c96d
Ania <dziewczynka.bez.zapalek@gmail.com>
2019-03-30 20:42:49 +0100

    Refactor entire script.js

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
 'use strict';
 
+
 // deklaracje
 
-var output1 = document.getElementById('converted');
-var output2 = document.getElementById('water');
-var output3 = document.getElementById('clothes');
+var output = document.getElementById('converted');
 
 var buttonCels = document.getElementById('Cels-button');
 var buttonFar = document.getElementById('Far-button');
 
+
 var givenData;
-var tempNumber;
-var tempCels;
-var tempFar;
+var celsium;
+var tempInFar;
 
+var messageCels = 'Podaj temperaturę w stopniach Celsjusza';
+var messageFar = 'Podaj temperaturę w stopniach Farenheita';
+var messageNaN = 'Nie podałeś liczby. Spróbuj raz jeszcze.';
 
-var waterInfo = function () {
-
-    if (tempCels <= 0) {
-        output2.innerHTML = 'Woda jest lodem - czyli jest w stanie stałym.'
-    } else if (tempCels > 0 && tempCels < 100) {
-        output2.innerHTML = 'Woda jest wodą - czyli jest w stanie ciekłym.'
-    } else if (tempCels >= 100) {
-        output2.innerHTML = 'Woda jest parą wodną - czyli jest w stanie lotnym.'
-    }
-}
-
-var clothesInfo = function () {
-
-    if (tempCels >= -65 && tempCels < 5) {
-        output3.innerHTML = 'Koniecznie zabierz czapkę i szalik!'
-    } else if (tempCels >= 5 && tempCels < 20) {
-        output3.innerHTML = 'Nie zapomnij o lekkiej kurtce.'
-    } else if (tempCels >= 20 && tempCels < 55) {
-        output3.innerHTML = 'Możesz założyć krótkie spodenki :)  '
-    } else {
-        output3.innerHTML = 'Czas pożyczyć skafander od NASA ;)  '
-    }
-}
 
 var reset = function () {
-    output1.innerHTML = '';
-    output2.innerHTML = '';
-    output3.innerHTML = '';
+    output.innerHTML = '';
 }
+
+var print = function (string) {
+    output.innerHTML += string + '<br>';
+}
+
+
+var giveANumber = function (message) {
+    return parseFloat(window.prompt(message));
+}
+
+
+var convertCelsToFar = function (input) {
+    return input * 1.8 - 32;
+}
+
+var convertFarToCels = function (input) {
+    return (input - 32) / 1.8;
+}
+
+var waterInfo = function (temp) {
+    if (temp <= 0) {
+        print('Woda jest lodem - czyli jest w stanie stałym.')
+    } else if (temp > 0 && temp < 100) {
+        print('Woda jest wodą - czyli jest w stanie ciekłym.')
+    } else if (temp >= 100) {
+        print('Woda jest parą wodną - czyli jest w stanie lotnym.')
+    }
+}
+
+var clothesInfo = function (temp) {
+    if (temp >= -65 && temp < 5) {
+        print('Koniecznie zabierz czapkę i szalik!')
+    } else if (temp >= 5 && temp < 20) {
+        print('Nie zapomnij o lekkiej kurtce.')
+    } else if (temp >= 20 && temp < 55) {
+        print('Możesz założyć krótkie spodenki :)  ')
+    } else {
+        print('Czas pożyczyć skafander od NASA ;)  ')
+    }
+}
+
 
 
 // mechanika
 
+
 buttonCels.addEventListener('click', function () {
 
     reset();
+    givenData = giveANumber(messageCels);
 
-    givenData = window.prompt('Podaj temperaturę w stopniach Celsjusza');
-    tempNumber = parseFloat(givenData);
-
-
-    if (isNaN(tempNumber)) {
-
-        output1.innerHTML = 'Nie podałeś liczby. Spróbuj raz jeszcze.'
+    if (isNaN(givenData)) {
+        print(messageNaN);
 
     } else {
 
-        tempCels = tempNumber;
-        tempFar = tempCels * 1.8 - 32;
+        celsium = givenData;
+        tempInFar = convertCelsToFar(givenData);
 
-        output1.innerHTML = 'Mamy ' + tempCels + ' st. Celsjusza, czyli ' + tempFar.toFixed(1) + ' st. Farenheita.'
-
-        waterInfo();
-        clothesInfo();
+        print('Mamy ' + celsium + ' st. Celsjusza, czyli ' + tempInFar.toFixed(1) + ' st. Farenheita.');
+        waterInfo(celsium);
+        clothesInfo(celsium);
     }
 
 });
@@ -77,18 +91,17 @@ buttonCels.addEventListener('click', function () {
 buttonFar.addEventListener('click', function () {
 
     reset();
+    givenData = giveANumber(messageFar);
 
-    givenData = window.prompt('Podaj temperaturę w stopniach Farenheita');
-    tempNumber = parseFloat(givenData);
+    if (isNaN(givenData)) {
+        print(messageNaN);
 
-    if (isNaN(tempNumber)) {
-        output1.innerHTML = 'Nie podałeś liczby. Spróbuj raz jeszcze.'
     } else {
 
-        tempCels = (tempFar - 32) / 1.8;
-        output1.innerHTML = 'Mamy ' + tempFar + ' st. Farenheita, czyli ' + tempCels.toFixed(1) + ' st. Celsjusza.'
+        celsium = convertFarToCels(givenData);
 
-        waterInfo();
-        clothesInfo();
+        print('Mamy ' + givenData + ' st. Farenheita, czyli ' + celsium.toFixed(1) + ' st. Celsjusza.');
+        waterInfo(celsium);
+        clothesInfo(celsium);
     }
 });
